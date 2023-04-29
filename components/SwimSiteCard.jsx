61406e49005686ea5cb367996ba54c1a67f990fb
Kirstin <kpetersen@elon.edu>
2023-04-29 18:53:20 +0100

    add and delete to favorite sites

--- components/SwimSiteCard.jsx
+++ components/SwimSiteCard.jsx
@@ -3,6 +3,8 @@ import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
 import { DEV_API_URL } from "../consts";
 import { useState } from "react";
+import axios from "axios";
+import Heart from "react-animated-heart";
 
 const SwimSiteCard = ({
   description,
@@ -13,23 +15,100 @@ const SwimSiteCard = ({
   parking_info,
   postcode,
   region,
+  isAccountCard,
+  favoriteId,
 }) => {
+  const token = localStorage.getItem("token");
+  const userId = localStorage.getItem("userId");
+  const [isClick, setClick] = useState(false);
+
+  const addToFavorites = async (e) => {
+    try {
+      const response = await axios.post(
+        `${DEV_API_URL}/favorites/`,
+        {
+          site: swimSitesId,
+          created_by: userId,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const deleteFromFavorites = async (e) => {
+    try {
+      const deletedFavorite = await axios.delete(
+        `${DEV_API_URL}/favorites/${favoriteId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (deletedFavorite) {
+        window.location.reload();
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
-      <Card style={{ width: "20rem" }}>
-        <Card.Img variant="top" src={image} />
-        <Card.Body>
-          <Card.Title>{name}</Card.Title>
-          <Card.Text>{location}</Card.Text>
-          <Button
-            variant="primary"
-            as={Link}
-            to={`/swim-sites/${swimSitesId}/`}
-          >
-            View More Details
-          </Button>
-        </Card.Body>
-      </Card>
+      {isAccountCard ? (
+        <Card style={{ width: "20rem" }}>
+          <Card.Body>
+            <Card.Title>{name}</Card.Title>
+            <Button variant="outline-danger" onClick={deleteFromFavorites}>
+              x
+            </Button>
+          </Card.Body>
+          <Card.Img variant="top" src={image} />
+          <Card.Body>
+            <Card.Title>{name}</Card.Title>
+            <Card.Text>{location}</Card.Text>
+            <Button
+              variant="primary"
+              as={Link}
+              to={`/swim-sites/${swimSitesId}/`}
+            >
+              View More Details
+            </Button>
+          </Card.Body>
+        </Card>
+      ) : (
+        <Card style={{ width: "20rem" }}>
+          <Card.Img variant="top" src={image} />
+          <Card.Body>
+            <Card.Title>
+              {name}{" "}
+              <Heart
+                isClick={isClick}
+                onClick={() => {
+                  setClick(!isClick);
+                  addToFavorites();
+                }}
+              />
+            </Card.Title>
+
+            <Card.Text>{location}</Card.Text>
+            <Button
+              variant="primary"
+              as={Link}
+              to={`/swim-sites/${swimSitesId}/`}
+            >
+              View More Details
+            </Button>
+          </Card.Body>
+        </Card>
+      )}
     </div>
   );
 };
--- pages/MyAccount.jsx
+++ pages/MyAccount.jsx
@@ -1,5 +1,48 @@
+import axios from "axios";
+import { DEV_API_URL } from "../consts";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import SwimSiteCard from "../components/SwimSiteCard";
+
 const MyAccount = () => {
-  return <p>You've made it to your My Acount page.</p>;
+  const { id } = useParams();
+  const [favorites, setFavorites] = useState([]);
+  console.log(id);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(`${DEV_API_URL}/auth/user/${id}`);
+        console.log(data.favorites);
+        const favorites = data.favorites;
+        console.log(favorites);
+        setFavorites(favorites);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <p>You've made it to your My Acount page.</p>
+      {favorites.map(({ site, id }) => {
+        return (
+          <li key={id}>
+            <SwimSiteCard
+              name={site.name}
+              location={site.location}
+              image={site.image}
+              swimSitesId={site.id}
+              isAccountCard={true}
+              favoriteId={id}
+            />
+            ;
+          </li>
+        );
+      })}
+    </div>
+  );
 };
 
 export default MyAccount;
